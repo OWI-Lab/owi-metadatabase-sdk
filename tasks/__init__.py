c3ed39a452ed83b951130fd6b0e5a02dbed24b05
Pietro D'Antuono <pietro.dantuono@24sea.eu>
2026-03-18 18:58:27 +0100

    ci(tasks): Remove useless invoke tasks

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,17 +1,10 @@
-from invoke import Collection
+from invoke.collection import Collection
 
-from . import ado, cruft, docs, performance, quality, search, test
+from . import docs, performance, quality, search, test
 
 ns = Collection()
 ns.add_collection(quality, name="qa")
 ns.add_collection(docs)
 ns.add_collection(test)
-ns.add_collection(ado)
-ns.add_collection(cruft)
 ns.add_collection(search)
 ns.add_collection(performance)
-
-
-from . import notebook
-
-ns.add_collection(notebook, name="nb")
